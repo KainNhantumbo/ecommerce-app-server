551bc5ac7109f5078dc94468988b5e3a001c2ffe
Kain Nhantumbo <nhantumbok@gmail.com>
2023-11-19 13:03:45 +0200

    feat: added .env configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/api/v1');
   app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 
